bfdb1cd7c622665ebf5205f589663ba922645649
Aisha <aishatellili@gmail.com>
2022-10-07 22:25:50 +0300

    test

--- test/lesson4/task1/Tests.kt
+++ test/lesson4/task1/Tests.kt
@@ -244,5 +244,6 @@ class Tests {
         assertEquals("двести тысяч два", russian(200002))
         assertEquals("девятьсот тысяч", russian(900000))
         assertEquals("двенадцать", russian(12))
+        assertEquals("четыреста пятьдесят одна тысяча пятьсот восемьдесят четыре", russian(451584))
     }
 }
